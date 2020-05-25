089fe02ac3017640a576c2e706569e56b2b83a5e
chrobles <chrobles4@gmail.com>
2020-05-25 15:19:52 -0400

    Sampled colors, move camera to class.

--- InOneWeekend/main.cpp
+++ InOneWeekend/main.cpp
@@ -1,5 +1,6 @@
 #include "Common.h"
 
+#include "Camera.h"
 #include "Sphere.h"
 #include "SurfaceList.h"
 
@@ -17,26 +18,16 @@ Color3 RayColor(const Ray &r, const Surface &world) {
 
 int main() {
   // Image dimensions
-  const double aspectRatio = 2.0 / 1.0;
+  const double aspectRatio = 16.0 / 9.0;
 
-  const int imageWidth = 800;
+  const int imageWidth = 384;
   const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
+
+  const int samplesPerPixel = 100;
 
   std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
 
-  // Viewport dimensions
-  const double viewportWidth = 4.0;
-  const double viewportHeight = viewportWidth / aspectRatio;
-  const double focalLength = 1.0;
-
-  // Coordinate origin
-  const Point3 origin = Point3(0, 0, 0);
-
-  // Viewport coordinates
-  const Vec3 viewportX = Vec3(viewportWidth, 0, 0);
-  const Vec3 viewportY = Vec3(0, viewportHeight, 0);
-  const Point3 viewportLL =
-      origin - viewportX / 2 - viewportY / 2 - Vec3(0, 0, focalLength);
+  Camera cam;
 
   SurfaceList world;
   world.Add(std::make_shared<Sphere>(Point3(0, 0, -1), 0.5));
@@ -45,12 +36,14 @@ int main() {
   for (int j = imageHeight - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < imageWidth; ++i) {
-      double u = double(i) / (imageWidth - 1);
-      double v = double(j) / (imageHeight - 1);
-
-      Ray r(origin, viewportLL + u * viewportX + v * viewportY - origin);
-      Color3 pixelColor = RayColor(r, world);
-      WriteColor(std::cout, pixelColor);
+      Color3 pixelColor(0.0, 0.0, 0.0);
+      for (int s = 0; s < samplesPerPixel; ++s) {
+        auto u = (i + Random()) / (imageWidth - 1);
+        auto v = (j + Random()) / (imageHeight - 1);
+        Ray r = cam.GetRay(u, v);
+        pixelColor += RayColor(r, world);
+      }
+      WriteColor(std::cout, pixelColor, samplesPerPixel);
     }
   }
   std::cerr << "\nDone.\n";
